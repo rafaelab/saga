8c034c8e438b01b97b6e54d87cdb1d2620cd71f2
Rafael Alves Batista <8rafael@gmail.com>
2015-07-31 02:33:26 -0300

    generation of source list from AMR grid

---
+++ utilities/GetListOfSources.cpp
@@ -0,0 +1,76 @@
+/*
+Creates a text file containing a list of "sources".
+Sources are defined as regions with density higher than rho0.
+*/
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+
+#include "saga/AMRcell.h"
+#include "saga/AMRgrid.h"
+#include "saga/LocalProperties.h"
+#include "saga/MagneticField.h"
+#include "saga/SQLiteInterface.h"
+#include "saga/Referenced.h"
+
+void Usage(std::string name)
+{
+    std::cout << "Running tests." << std::endl;
+    std::cout << "USAGE" << std::endl;
+    std::cout << "./" << name << " <path_to_SQL_file> arg.... " <<  std::endl;    
+    std::cout << "  arg 1: path to SQL file containing the magnetic field and density" << std::endl;
+    std::cout << "  arg 2: name of output text file" << std::endl;
+    std::cout << "  arg 3: critical density (rho0) [suggested: 1e-29]" << std::endl;
+    std::cout << "  arg 4: length conversion factor [to m]" << std::endl;
+    std::cout << "  arg 5: box origin x-coordinate [use 0 for grid units]" << std::endl;
+    std::cout << "  arg 6: box origin y-coordinate [use 0 for grid units]" << std::endl;
+    std::cout << "  arg 7: box origin z-coordinate [use 0 for grid units]" << std::endl;
+    std::cout << "  arg 8: conversion factor density" << std::endl;
+}
+
+int main(int argc, char** argv )
+{
+
+    if(argc != 9)
+    {
+        Usage(argv[0]);
+        return -1;
+    }
+    std::string filename = argv[1];
+    std::string outputfile = argv[2];
+    double rho0 = atof(argv[3]);
+    double cl = atof(argv[4]);
+    double x0 = atof(argv[5]);
+    double y0 = atof(argv[6]);
+    double z0 = atof(argv[7]);
+    double crho = atof(argv[8]);
+
+    std::cout << "Input file: " << filename << std::endl;
+    saga::ref_ptr<saga::AMRgrid> amr = new saga::AMRgrid(filename, 10);
+    std::cout << "Input file opened." << std::endl;
+
+    std::cout << "Critical density = " << rho0 << std::endl;
+    std::cout << "Output file: " << outputfile << std::endl;
+    std::ofstream fout(outputfile.c_str());
+
+    int nCells = amr->getGridSize();
+    std::cout << "Number of cells: " << nCells << std::endl;
+    for(int i=1; i<nCells; i++) {
+        saga::AMRcell cell = amr->getCellWithIndex(i);
+        double x = (cell.getXcenter() + x0) * cl;
+        double y = (cell.getYcenter() + y0) * cl;
+        double z = (cell.getZcenter() + z0) * cl;
+        // std::cout << i << " " << x << " " << y << " " << z << std::endl;
+        saga::LocalProperties lp = amr->getLocalPropertiesFromIndex(i);
+        double rho = lp.getDensity() * crho;
+        if (rho > rho0) {
+            // std::cout << i << "\t" << rho/rho0 << "\t" << x << "\t" << y << "\t" << z << std::endl;
+            fout << x << "\t" << y << "\t" << z << std::endl;
+        }
+    }
+
+    fout.close();
+
+    return 0;
+}
